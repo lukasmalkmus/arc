7fcf41f0858a6fb8ca68f6a90df8ef35a6e7f804
Lukas Malkmus <mail@lukasmalkmus.com>
2017-03-21 01:16:22 +0100

    Dummy assembler

--- build/build.go
+++ build/build.go
@@ -1,1 +1,20 @@
 package build
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/LukasMa/arc/parser"
+)
+
+// Assemble will transform ARC source code into machine code. The function takes
+// an io.Reader as source for the ARC source code. It writes the assembled
+// program to an io.Writer destination.
+func Assemble(src io.Reader) (io.Writer, error) {
+	p := parser.New(src)
+	prog, err := p.Parse()
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewBufferString(prog.String()), nil
+}
